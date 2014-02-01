4c67018cb90cb500ba7ef4167a015bfcb3a721df
Max Howell <mxcl@me.com>
2014-02-01 13:22:02 -0500

    v1

---
+++ UIImage+WithColor.m
@@ -0,0 +1,19 @@
+//mxclmade
+
+#import "UIImage+WithColor.h"
+
+
+@interface UIImage (WithColor)
+
++ (UIImage *)imageWithColor:(UIColor *)color {
+    CGRect rect = (CGRect){.size = {1, 1}};
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return img;
+}
+
+@end
